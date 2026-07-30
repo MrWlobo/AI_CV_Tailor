ff43646c797bfb886f6e03a4d223fe537b4a620b
MrWlobo <michaldworniczak40@gmail.com>
2026-07-30 20:33:01 +0200

    Corrected sample_pdf_bytes fixture to be a valid pdf

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,7 +11,14 @@ def api_client():
 
 @pytest.fixture
 def sample_pdf_bytes():
-    return b"%PDF-1.4 sample content for testing"
+    return (
+        b"%PDF-1.4\n"
+        b"1 0 obj<</Type/Catalog/Pages 2 0 R>>endobj\n"
+        b"2 0 obj<</Type/Pages/Count 1/Kids[3 0 R]>>endobj\n"
+        b"3 0 obj<</Type/Page/MediaBox[0 0 300 144]/Parent 2 0 R/Resources<<>>>>endobj\n"
+        b"xref\n0 4\n0000000000 65535 f\n0000000009 00000 n\n0000000052 00000 n\n0000000101 00000 n\n"
+        b"trailer<</Size 4/Root 1 0 R>>\nstartxref\n178\n%%EOF\n"
+    )
 
 
 @pytest.fixture
