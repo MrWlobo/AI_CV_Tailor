89e8ee62b6079652a4e48cc801cf9bc0bbfa046a
MrWlobo <michaldworniczak40@gmail.com>
2026-07-31 20:16:11 +0200

    Added basic error handling to endpoint

--- src/backend/api.py
+++ src/backend/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 import io
 from pypdf import PdfReader
 from backend.llm_integration import get_tailored_results
@@ -20,26 +20,30 @@ def health_check():
 
 @app.post("/tailor")
 async def tailor_cv(cv_file: UploadFile = File(...), job_offer: str = Form(...)):
-    # Get CV content as bytes
-    cv_bytes = await cv_file.read()
+    try:
+        # Get CV content as bytes
+        cv_bytes = await cv_file.read()
 
-    # Transform bytes to readable content
-    pdf_stream = io.BytesIO(cv_bytes)
-    reader = PdfReader(pdf_stream)
-    cv_text = "".join([page.extract_text() or "" for page in reader.pages])
+        # Transform bytes to readable content
+        pdf_stream = io.BytesIO(cv_bytes)
+        reader = PdfReader(pdf_stream)
+        cv_text = "".join([page.extract_text() or "" for page in reader.pages])
 
-    # Call LLM to make its assessments
-    tailored_results = get_tailored_results(cv_text, job_offer)
+        # Call LLM to make its assessments
+        tailored_results = get_tailored_results(cv_text, job_offer)
 
-    status = tailored_results["status"]
-    tailored_cv = tailored_results["tailored_cv"]
-    match_score = tailored_results["match_score"]
-    recommendations = tailored_results["recommendations"]
+        status = tailored_results["status"]
+        tailored_cv = tailored_results["tailored_cv"]
+        match_score = tailored_results["match_score"]
+        recommendations = tailored_results["recommendations"]
 
-    # Return results to frontend
-    return {
-        "status": status,
-        "tailored_cv": tailored_cv,
-        "match_score": match_score,
-        "recommendations": recommendations,
-    }
+        # Return results to frontend
+        return {
+            "status": status,
+            "tailored_cv": tailored_cv,
+            "match_score": match_score,
+            "recommendations": recommendations,
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
