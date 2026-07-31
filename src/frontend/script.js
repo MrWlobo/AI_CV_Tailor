8386281c29e12d79e96def16da001b68771c14be
MrWlobo <michaldworniczak40@gmail.com>
2026-07-31 14:37:14 +0200

    Output section will now be hidden by default and revealed by clicking submit button

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,7 +1,13 @@
 // HTML Elements
-const dropZone = document.getElementById('drop-zone');
-const fileInput = document.getElementById('cv-file');
-const fileNameDisplay = document.getElementById('drop-file-name');
+const dropZone = document.getElementById("drop-zone");
+const fileInput = document.getElementById("cv-file");
+const fileNameDisplay = document.getElementById("drop-file-name");
+const submitButton = document.getElementById("submit-button");
+const outputDiv = document.getElementById("output");
+const tailoredCvParagraph = document.getElementById("tailored-cv-paragraph");
+const recommendationsParagraph = document.getElementById("recommendations-paragraph");
+const chart = document.getElementById("match-chart");
+const percentageText = document.getElementById("chart-percentage");
 
 // Event Listeners
 
@@ -39,14 +45,21 @@ dropZone.addEventListener("drop", (e) => {
     }
 });
 
-// Match Doughnut Chart
+// Update HTML
 function updateMatchChart(score) {
-    const chart = document.getElementById('match-chart');
-    const percentageText = document.getElementById('chart-percentage');
-
     percentageText.textContent = `${score}%`;
-
     chart.style.setProperty('--percentage', `${score}%`);
 }
 
-updateMatchChart(80);
+function updateTailoredCv(tailoredCv) {
+    tailoredCvParagraph.textContent = tailoredCv;
+}
+
+function updateRecommendations(recommendations) {
+    tailoredCvParagraph.textContent = recommendations.join(", ");
+}
+
+// Submit Button
+submitButton.addEventListener("click", () => {
+    outputDiv.classList.remove("hidden");
+})
