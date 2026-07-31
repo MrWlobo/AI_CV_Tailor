743acda492aa119ba7888959626d48e4541c4fd7
MrWlobo <michaldworniczak40@gmail.com>
2026-07-31 10:00:11 +0200

    Initialized model and added system prompt

--- src/backend/llm_integration.py
+++ src/backend/llm_integration.py
@@ -1,6 +1,5 @@
-import requests
 from dotenv import load_dotenv
-from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -8,3 +7,24 @@ dotenv_path = BASE_DIR / ".env"
 
 load_dotenv(dotenv_path=dotenv_path)
 
+system_prompt = """
+    You are an AI CV tailor. You will receive a transcript of a CV and a job offer.
+    Your task is to grade how well the CV matches the offer on a scale 0-100, return
+    an improved version of CV and provide the user with some concise recommendations.
+
+    You MUST return ONLY the json of a following structure as your response:
+    {
+        "status": "success" or "failure",
+        "tailored_cv": improved CV as a string,
+        "match_score": integer from 0 to 100 including 0 and 100,
+        "recommendations": short reccomendations for the user as a list of strings,
+    }
+    DO NOT return any additional messages.
+    """
+
+model = init_chat_model(
+    model="gemini-2.5-flash",
+    model_provider="google_genai",
+    temperature=0.1,
+    system_prompt=system_prompt,
+)
